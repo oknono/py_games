6fb527ab835c6ad528d93f0381c6bcd2f97c1b9b
Kirsten <kirsten@eml.cc>
2014-11-22 15:39:22 +0700

    Added play again while loop

--- dragon.py
+++ dragon.py
@@ -1,13 +1,11 @@
 import random
 import time
 
-
-
 def displayIntro():
-	print(''' You are in a land filled with MONSTERS. In front
+	print(''' \nYou are in a land filled with MONSTERS. In front
 of you you see two caves. One cave houses a FRIENDLY monster,
 that will share cake and balloons with you. The OTHER cave 
-though... houses an EVIL monster that will eat you on sight.''')
+though... houses an EVIL monster that will eat you on sight.\n''')
 
 	
 def chooseCave():
@@ -23,19 +21,26 @@ def checkCave(chosenCave):
 	time.sleep(3)
 	print('It is dark and spooky...')
 	time.sleep(3)
-	print('A large monster out in front of you! He opens his jaws and...')
-	print("\n")
+	print('A large monster out in front of you! He opens his jaws and... \n')
 	time.sleep(2)
 	
 	friendlyCave = random.randint(1,2)
 	
 	if chosenCave == friendlyCave:
-		print("Showers you with champagne! Good choice young one!")
+		print("Showers you with champagne! Good choice young one!\n")
 	else:
-		print("EATS YOU UP AND SPITS YOU OUT!")
+		print("EATS YOU UP AND SPITS YOU OUT!\n")
 
-displayIntro()
-chooseCave()
+play_again = True
 
-# Add play_again thing
+while play_again == True:
+
+	displayIntro()
+	chooseCave()
+	play = raw_input("Do you want to play again? (y)es or (n)o: ")
+	if play == 'yes' or play == 'y':
+		play_again = True
+	else:
+		play_again = False
+
 
