415e41578e6e58bd86a35fd0156b9c50a179191f
Kirsten <kirsten@eml.cc>
2014-12-02 15:40:26 +0700

    Added player choice and begin of win heuristic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,9 @@
 # Introduction
-print " Welcome to Tic Tac Toe! \n"
+
+play_again = True
 
 def print_example_board():
-    print "Please enter the number where you want to play. See the illustration below"
+    print "To play, please enter the number where you want to play. See the illustration below"
     EXAMPLE_BOARD ='''
    |   |   
  1 | 2 | 3
@@ -35,12 +36,41 @@ def print_board(v1, v2, v3, v4 ,v5, v6, v7, v8, v9):
 '''%(v1,v2,v3,v4,v5,v6,v7,v8,v9)
     print BOARD
     
-print_example_board()
 
-print_board('X',' ','X','O',' ',' ','0',' ',' ')
+while play_again:
+    print " Welcome to Tic Tac Toe! \n"
+    print_example_board()
+    print_board('X',' ','X','O',' ',' ','0',' ',' ')
+    # Let player choose symbol
+    while True:
+        letter = raw_input("Do you want to play \"X\" or \"O\"? :").upper() 
+        if letter != "X" and letter !="O":
+        	print "Does not compute, please choose again"
+        else:
+            break
+    print "player symbol is %s"%letter
+    play_again = raw_input("Do you want to play again?(Y)es/(N)o: ").lower().startswith('y')
 
-	
+    
+
+    
 
 
+
+#check for win	
+#row_win
+#v1 == v2 == v3 
+#v4 == v5 == v6
+#v7 == v8 == v9
+
+#column_win
+
+#v1 == v4 == v7
+#v2 == v5 == v8
+#v3 == v6 == v9
+
+#diagonal_win
+#v1 == v5 == v9
+#v3 == v5 == v7
 
 
