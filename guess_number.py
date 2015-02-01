d10d867cc3b8be25bc8154e06d86c120666a415c
Kirsten Korevaar <kirsten@eml.cc>
2015-02-01 14:04:23 +0700

    replace tab with space, clean up syntax

--- guess_number.py
+++ guess_number.py
@@ -5,22 +5,22 @@ guess_total = 0
 
 name = raw_input("What is your name? ")
 
-print("Hello %s ! Please guess the number between 1 and 20. You get 6 tries" %name)
+print "Hello %s ! Please guess the number between 1 and 20. You get 6 tries" %name
 
 while guess_total < 6 :
-	guess = int(raw_input("take a guess: " ))
-	guess_total += 1
+    guess = int(raw_input("Take a guess: " ))
+    guess_total += 1
 	 
-	if guess < computer_guess:
-		print("Too low!")
-	elif guess > computer_guess:
-		print("Too high!")
-	elif guess == computer_guess:
-		break
-	else:
-		print("oops, something went wrong")
+    if guess < computer_guess:
+        print "Too low!"
+    elif guess > computer_guess:
+        print "Too high!"
+    elif guess == computer_guess:
+        break
+    else:
+        print "Oops, something went wrong" 
 		
 if guess == computer_guess:
-	print("YESSSSS %s, you won, and it only took you %s guesses" %(name,guess_total))
+    print "YESSSSS %s, you won, and it only took you %s guesses!" %(name,guess_total)
 else:
-	print("You lose, the number I was thinking of was %s" s%computer_guess)
+    print "You lose, the number I was thinking of was %s" %computer_guess
